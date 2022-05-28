7461355909edd6f6f93171634e0dceb2771295a4
TakaoJINNO <62533161+jinno-oit@users.noreply.github.com>
2022-05-28 15:25:12 +0900

    Update handsDemo.py

--- handsDemo.py
+++ handsDemo.py
@@ -4,22 +4,30 @@ from MediapipeClass import MediapipeHands
 def main():
     cap = cv2.VideoCapture(0)
     mph = MediapipeHands()
+    mode = 1
     while cap.isOpened():
         ret, frame = cap.read()
         if ret is None:
             print('cannot read frame.')
 
+        key = cv2.waitKey(1)&0xFF
+        if key >= ord('0') and key <= ord('9'):
+            mode = key - ord('0')
+        
         frame = cv2.flip(frame, 1)
         hands = mph.detectHands(frame)
 
-        # print(hands.Left.getPoint(8))
-        # hands.Left.judgeOpen('thumb')
-        # hands.Right.judgeAll()
+        if mode == 1:
+            print(hands.Left.getPoint(1, 8))
+
+        elif mode == 2:
+            hands.Left.judgeOpen(finger_id='thumb')
+
+        elif mode == 3:
+            hands.Right.judgeAll()
 
         mph.drawHandAll(frame, hands)
-
         cv2.imshow('res', frame)
-        key = cv2.waitKey(1)&0xFF
 
         if key == ord('q'):
             break
